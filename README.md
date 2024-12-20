f35ec27256cdec3118644dbadeb2ea138284807a
Manirathinam21 <manirathinam21@gmail.com>
2024-12-20 22:52:11 +0530

    project setup done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ML_Pipeline_using_DVC
+
+## How to Run?
+
+conda create -n test python=3.11 -y
+
+conda activate test
+
+pip install -r requirements.txt
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MLPipelinewithDVC",
+    version= "0.0.0",
+    author="Manirathinam21",
+    author_email='manirathinam21@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
